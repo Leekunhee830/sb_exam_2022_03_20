57834e8aa4f5d04ee8f54e70affae5d8b9d979a3
Leekunhee830 <gorrka159@naver.com>
2022-03-20 16:38:54 +0900

    폴더 모듈화, 주석설명

---
+++ src/main/java/com/lkh/exam1/user/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.lkh.exam1.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api")
+public class UserController {
+    @RequestMapping("/users")
+    @ResponseBody
+    public String users(){
+        return "유저 여러명";
+    }
+
+    @RequestMapping("/user")
+    @ResponseBody
+    public String user(){
+        return "유저 한명";
+    }
+}
